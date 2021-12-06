9342d8b4fff30bf7882b80b59b12a86da5d00077
Davidh <davidharvey_@hotmail.com>
2021-12-06 17:24:26 +0000

    random quote working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,103 @@
+import axios from 'axios'
+import React from 'react'
+
+
 function App() {
-  return <h1>Hello World</h1>
+ 
+  const [quotes, setQuotes] = React.useState(null)
+  const [searchValue, setSearchvalue] = React.useState('')
+
+  React.useEffect(() => {
+    const pullData = async () => {
+      const quoteData = await axios.get('https://type.fit/api/quotes')
+      setQuotes(quoteData.data)
+    }
+    pullData()
+  }, [])
+
+  const handleSearch = (e) => {
+    setSearchvalue(e.target.value)
+  }
+
+  let randomNumber = 1
+  if (quotes) {
+    randomNumber = Math.floor(Math.random() * quotes.length)
+    console.log(randomNumber)
+  }
+  // const filteredQuotes = (quotes) => {
+  //   return quotes.filter(quote => {
+  //     return quote.author.toLowerCase().includes(searchValue.toLowerCase())  || 
+  //   })
+  // }
+
+  
+  return (
+    <>
+      <section>
+        <input className='search-box'
+          placeholder='Quote search'
+          onChange={handleSearch}>
+        </input>
+      </section>
+      <section>
+        {quotes ? (
+          <div>
+            <h4>{quotes[randomNumber].text}</h4>
+            <p>{quotes[randomNumber].author}</p>
+          </div>
+          
+        ) : (
+          <p>loading...</p>
+        )
+        }
+      </section>
+    </>
+  )
 }
 
 export default App
+
+
+// const [quotes, setQuotes] = React.useState({
+//   text: '',
+//   author: '',
+// })
+
+// const [searchValue, setSearchvalue] = React.useState('')
+
+// React.useEffect(() => {
+//   const pullData = async () => {
+//     const quoteData = await axios.get('https://type.fit/api/quotes')
+//     setQuotes(quoteData.data)
+//   }
+//   pullData()
+// }, [])
+
+// // const handleSearch = (e) => {
+// //   setSearchvalue(e.target.value)
+// // }
+
+// // const filteredQuotes = (quotes) => {
+// //   const theFilteredQuote = quotes.filter(quote => {
+// //     return quote.author.toLowerCase().includes(searchValue.toLowerCase())
+// //   })
+// // }
+
+// console.log('type', typeof quotes)
+// console.log('quotes', quotes.length)
+// // console.log(theFilteredQuote)
+
+// return (
+//   <section>
+//     {/* <input className='search-box'
+//       placeholder='Quote search'
+//       onChange={handleSearch}>
+//     </input> */}
+//       {quotes.map(quote => (
+//         <div key={quote.text}>
+//           <p>{quote.author}</p>
+//           <p>{quote.author}</p>
+//           </div>
+//       ))}
+//   </section>
+// )
---
+++ src/components/SearchFunction.js
@@ -0,0 +1,8 @@
+function SearchFunction() {
+
+  return (
+    <h1>hll</h1>
+  )
+}
+
+export default SearchFunction
